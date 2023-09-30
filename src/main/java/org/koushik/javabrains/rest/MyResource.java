b375ac50cabba85a14608e8b64a0e0a3e0386ac1
tothm23 <tothm.113sz@acsjszki.hu>
2023-09-30 18:38:12 +0200

    Advanced JAX-RS 07 - Paraméter megjegyzések és tagváltozók

--- src/main/java/org/koushik/javabrains/rest/MyResource.java
+++ src/main/java/org/koushik/javabrains/rest/MyResource.java
@@ -3,19 +3,23 @@ package org.koushik.javabrains.rest;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("test")
+@Path("{pathParam}/test")
 @Singleton
 public class MyResource {
 
-	private int count;
+	@PathParam("pathParam")
+	private String pathParamExample;
+	@QueryParam("queryParam")
+	private String queryParamExample;
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String testMethod() {
-		count = count + 1;
-		return "It works! This method was called " + count + " time(s)";
+		return "It works! Path param used " + pathParamExample + " and Query param used " + queryParamExample;
 	}
 }
