2feff39aa0b7f31719650c252e04ab0d30b2af42
tothm23 <tothm.113sz@acsjszki.hu>
2023-10-02 14:24:56 +0200

    Advanced JAX-RS 16 – Néhány bevált gyakorlat

--- src/main/java/org/koushik/javabrains/rest/client/RestApiClient.java
+++ src/main/java/org/koushik/javabrains/rest/client/RestApiClient.java
@@ -2,6 +2,7 @@ package org.koushik.javabrains.rest.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 import org.koushik.javabrains.messenger.model.Message;
@@ -11,17 +12,15 @@ public class RestApiClient {
 	public static void main(String[] args) {
 
 		Client client = ClientBuilder.newClient();
-		
-		// A válasz az osztály példánya
-		Message messageMessage = client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/1")
-				.request(MediaType.APPLICATION_JSON).get(Message.class);
-		
-		// A válasz egyszerű szöveg
-		String messageString = client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/1")
-				.request(MediaType.APPLICATION_JSON).get(String.class);
 
-		System.out.println(messageMessage);
-		System.out.println(messageString);
+		WebTarget baseTarget = client.target("http://localhost:8080/advanced-jaxrs-01/webapi/");
+		WebTarget messagesTarget = baseTarget.path("messages");
+		WebTarget singleMessageTarget = messagesTarget.path("{messageId}");
+
+		Message message = singleMessageTarget.resolveTemplate("messageId", "2").request(MediaType.APPLICATION_JSON)
+				.get(Message.class);
+
+		System.out.println(message);
 	}
 
 }
