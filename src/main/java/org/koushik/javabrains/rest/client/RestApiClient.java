7d50892b71645f7f0302a81b363a112e4fb74537
tothm23 <tothm.113sz@acsjszki.hu>
2023-10-02 12:55:55 +0200

    Advanced JAX-RS 15 – Az ügyfélkód lépésről lépésre magyarázata

--- src/main/java/org/koushik/javabrains/rest/client/RestApiClient.java
+++ src/main/java/org/koushik/javabrains/rest/client/RestApiClient.java
@@ -2,7 +2,7 @@ package org.koushik.javabrains.rest.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 import org.koushik.javabrains.messenger.model.Message;
 
@@ -11,9 +11,17 @@ public class RestApiClient {
 	public static void main(String[] args) {
 
 		Client client = ClientBuilder.newClient();
-		Response response = client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/1").request().get();
-		Message message = response.readEntity(Message.class);
-		System.out.println(message.getMessage());
+		
+		// A válasz az osztály példánya
+		Message messageMessage = client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/1")
+				.request(MediaType.APPLICATION_JSON).get(Message.class);
+		
+		// A válasz egyszerű szöveg
+		String messageString = client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/1")
+				.request(MediaType.APPLICATION_JSON).get(String.class);
+
+		System.out.println(messageMessage);
+		System.out.println(messageString);
 	}
 
 }
